7d591982935a0b978c11d2593077810ed611989f
Barle Armand <armand.barle2945@gmail.com>
2023-11-27 14:16:33 +0200

    Added circular diagram

--- latex/main.py
+++ latex/main.py
@@ -1,3 +1,5 @@
+import math
+
 from classes.Automaton import Automaton
 
 
@@ -114,52 +116,42 @@ def draw_states_single_line(automaton):
             previous_state = state
 
 
-def draw_states_circular(automaton):
+def draw_states_circular(automaton, rho=10):
+    """
+    draws states of the automaton in a circular shape
+    rho = radius
+    alpha = angle is in range [0, 2pi] and divided into as many points there are
+    x = rho * cos(alpha)
+    y = rho * sin(alpha)
+    :param automaton:
+    :return:
+    """
     with open("tikz_out.txt", 'w') as fout:
         # Initialization
         fout.write(
             "\\begin{tikzpicture}[->,>=stealth',shorten >=1pt,auto,node distance=3.5cm, scale = 1,transform "
             "shape]\n\n\n")
 
+        # create coordinates for the nodes
+        coords = []
+        for i, state in enumerate(automaton.states):
+            alpha = 2 * math.pi * i / len(automaton.states)
+            state_x = rho * math.cos(alpha)
+            state_y = rho * math.sin(alpha)
+            coords.append((-state_x, -state_y))
+
         # Initializes states
-        previous_state = None
-        for state in automaton.states:
+        for i, state in enumerate(automaton.states):
             line = "\\node[state"
 
             # first it checks if states are initial or final and adds tags acordingly
-            # creates first state
-            if state == automaton.states[0]:
-                if state.is_initial:
-                    line += ",initial"
-                if state.is_final:
-                    line += ",accepting"
-                line += "] (" + state.state_name + ") {$" + state.state_name + "$};\n\n\n"
-                previous_state = state
-                fout.write(line)
-                continue
             if state.is_initial:
                 line += ",initial"
             if state.is_final:
                 line += ",accepting"
-            line += "] (" + state.state_name + ")"
-
-            # after the automata wrote half of its states it starts in a new line
-            if len(automaton.states) % 2 == 0 and state == automaton.states[int(len(automaton.states) / 2)]:
-                line += "[below of=" + automaton.states[0].state_name + "] {$" + state.state_name + "$};\n\n\n"
-                previous_state = state
-                fout.write(line)
-                continue
-
-            if len(automaton.states) % 2 == 1 and state == automaton.states[int(len(automaton.states) // 2) + 1]:
-                line += "[below of=" + automaton.states[0].state_name + "] {$" + state.state_name + "$};\n\n\n"
-                previous_state = state
-                fout.write(line)
-                continue
-
-            # end of line if normal states always adds them to the right of the previous state
-            line += "[right of=" + previous_state.state_name + "] {$" + state.state_name + "$};\n\n\n"
+            # adds the name of the sate then the its coordinates and the state name which is displayed
+            line += "] (" + state.state_name + ") at " + str(coords[i]) + " {$" + state.state_name + "$};\n\n\n"
             fout.write(line)
-            previous_state = state
 
 
 def draw_transitions(automaton):
@@ -204,7 +196,7 @@ def draw_transitions(automaton):
                            "{" + ", ".join(str(letter) for letter in transition.letter) + "} " +
                            "(" + transition.target_state.state_name + ")\n")
 
-        fout.write("\n\n\n\n")
+        fout.write(";\n\n\n\n")
         # end of tikz
         fout.write("\\end{tikzpicture}")
 
@@ -258,12 +250,29 @@ def draw_transitions_straight_line(automaton):
                            "{" + ", ".join(str(letter) for letter in transition.letter) + "} " +
                            "(" + transition.target_state.state_name + ")\n")
 
-        fout.write("\n\n\n\n")
+        fout.write(";\n\n\n\n")
         # end of tikz
         fout.write("\\end{tikzpicture}")
 
 
+def draw_multiline(filename):
+    a = Automaton(filename)
+    draw_states_multiline(a)
+    draw_transitions(a)
+
+
+def draw_single_line(filename):
+    # should only be used for automata with less than 7 states
+    a = Automaton(filename)
+    draw_states_single_line(a)
+    draw_transitions_straight_line(a)
+
+
+def draw_circular(filename):
+    a = Automaton(filename)
+    draw_states_circular(a, 5)
+    draw_transitions(a)
+
+
 if __name__ == '__main__':
-    automaton = Automaton("tikz_hazi3")
-    draw_states_single_line(automaton)
-    draw_transitions_straight_line(automaton)
+    draw_circular("tikz2")
