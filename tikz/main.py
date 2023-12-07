7ec53959b4526a506d39878d5d5a95aaebd0b0a6
Barle Armand <armand.barle2945@gmail.com>
2023-12-07 17:55:13 +0200

    Updated main files

--- tikz/main.py
+++
@@ -1,6 +0,0 @@
-from tikz.CircularTikz import CircularTikz
-from tikz.LineTikz import LineTikz
-from tikz.MultilineTikz import MultilineTikz
-
-if __name__ == '__main__':
-    circular_tikz = MultilineTikz("input_files/tikz3")
